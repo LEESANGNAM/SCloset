b7f314a41384c5489e470cff1c38714bdf16d050
LEESANGNAM <fgh0116@naver.com>
2023-11-29 23:59:20 +0900

    [feat] 글작성 뷰 틀 완성

--- SCloset/Scene/Style/Add/View/StyleAddView.swift
+++ SCloset/Scene/Style/Add/View/StyleAddView.swift
@@ -8,13 +8,74 @@
 import UIKit
 
 class StyleAddView: BaseView {
+    let scrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+
+    let contentView = UIView()
+    lazy var contentList: [UIView] = [lookImageView,titleTextField,locationLabel,contentTextView]
     
-    
+    let lookImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .blue
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    let titleTextField = {
+        let view = UITextField()
+        view.placeholder = "제목을 입력해주세요~"
+        return view
+    }()
+    let locationLabel = {
+      let view = UILabel()
+        view.text = "위치: 날씨/날씨"
+        view.textColor = .lightGray
+        view.font = .systemFont(ofSize: 14)
+        return view
+    }()
+    let contentTextView = {
+        let view = UITextView()
+        view.text = "자ㅏ라라라라라라랜요오옹오오내요요요오오옹내요요요요오오옹"
+        return view
+    }()
     
     override func setHierarchy() {
-        
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentList.forEach { contentView.addSubview($0) }
     }
     override func setconstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.width.equalTo(scrollView)
+        }
+        
+        lookImageView.snp.makeConstraints { make in
+            make.top.equalTo(contentView.snp.top)
+            make.width.equalTo(contentView)
+            make.height.equalTo(self.safeAreaLayoutGuide).multipliedBy(0.6)
+        }
+        titleTextField.snp.makeConstraints { make in
+            make.top.equalTo(lookImageView.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview().inset(10)
+        }
+        locationLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleTextField.snp.bottom).offset(10)
+            make.horizontalEdges.equalToSuperview().inset(10)
+        }
+        contentTextView.snp.makeConstraints { make in
+            make.top.equalTo(locationLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalToSuperview().inset(10)
+            make.height.equalTo(self.safeAreaLayoutGuide).multipliedBy(0.9)
+            make.bottom.equalTo(contentView.snp.bottom)
+        }
+        
+        
         
     }
     
--- SCloset/Scene/Style/Add/View/StyleAddViewController.swift
+++ SCloset/Scene/Style/Add/View/StyleAddViewController.swift
@@ -8,49 +8,12 @@
 import UIKit
 
 class StyleAddViewController: BaseViewController {
-    let button = {
-        let button = UIButton()
-        button.setTitle("테스트으으으", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .black
-        return button
-    }()
-    let imageView = {
-        let view = UIImageView()
-        view.image = UIImage(named: "testImage")
-        return view
-    }()
+    let mainView = StyleAddView()
     let viewModel = StyleAddViewModel()
+    override func loadView() {
+        self.view = mainView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        
     }
-    
-    override func setConstraints() {
-        view.addSubview(imageView)
-        view.addSubview(button)
-        
-        
-        imageView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.size.equalTo(200)
-        }
-        button.snp.makeConstraints { make in
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
-            make.top.equalTo(imageView.snp.bottom).offset(40)
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-30)
-        }
-        
-        
-    }
-    
-    @objc func buttonTapped(){
-        if let image = imageView.image,
-           let data = image.jpegData(compressionQuality: 0.5){
-            viewModel.postUpLoad(data: data)
-        }
-    }
-    
 }
