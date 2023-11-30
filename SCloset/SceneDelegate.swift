d946b87f95ff6214576960283042c4a9714cd444
LEESANGNAM <fgh0116@naver.com>
2023-11-30 23:02:45 +0900

    [feat] weatherKit CLLocation  테스트

--- SCloset/SceneDelegate.swift
+++ SCloset/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         if UserDefaultsManager.isLogin {
             window = UIWindow(windowScene: scene)
-            let vc = TabbarController()
+            let vc = WeatherTestViewController()
             window?.rootViewController = vc
             window?.makeKeyAndVisible()
         } else {
