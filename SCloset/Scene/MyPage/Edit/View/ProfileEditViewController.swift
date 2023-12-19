d964642f8bf89e5f9afabfec77c25f3e25428ad9
LEESANGNAM <fgh0116@naver.com>
2023-12-20 01:24:04 +0900

    [feat] phpicker test

--- SCloset/Scene/MyPage/Edit/View/ProfileEditViewController.swift
+++ SCloset/Scene/MyPage/Edit/View/ProfileEditViewController.swift
@@ -5,11 +5,13 @@
 //  Created by 이상남 on 12/18/23.
 //
 
-import Foundation
+import UIKit
+import PhotosUI
 
 class ProfileEditViewController: BaseViewController {
     
     let mainView = ProfileEditView()
+    var picker: PHPickerViewController!
     
     override func loadView() {
         view = mainView
@@ -17,6 +19,46 @@ class ProfileEditViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setImageViewTapGesture()
+        setPHPicker()
+    }
+    
+    private func setImageViewTapGesture(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(lookImageViewTapped))
+        mainView.profileImageView.isUserInteractionEnabled = true
+        mainView.profileImageView.addGestureRecognizer(tapGesture)
+    }
+    @objc private func lookImageViewTapped(){
+        print(#function)
+        present(picker, animated: true)
+    }
+    private func setPHPicker(){
+        var phPickerConfiguration = PHPickerConfiguration()
+        phPickerConfiguration.filter = .any(of: [.images,.livePhotos])
+        picker = PHPickerViewController(configuration: phPickerConfiguration)
+        picker.delegate = self
+    }
+}
+
+extension ProfileEditViewController: PHPickerViewControllerDelegate{
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        let itemProvider = results.first?.itemProvider // 2
+        
+        if let itemProvider = itemProvider,
+           itemProvider.canLoadObject(ofClass: UIImage.self) { // 3
+            itemProvider.loadObject(ofClass: UIImage.self) {[weak self] (image, error) in // 4
+                DispatchQueue.main.async { [weak self] in
+                    self?.mainView.profileImageView.image = image as? UIImage
+//                    if let imageData = (image as? UIImage)?.jpegData(compressionQuality: 0.5) {
+//                        self?.viewModel.setImageData(imageData)
+//                    }
+//                    self?.mainView.plusImageView.isHidden = true
+                }
+            }
+        } else {
+            // TODO: Handle empty results or item provider not being able load UIImage
+        }
     }
     
 }
