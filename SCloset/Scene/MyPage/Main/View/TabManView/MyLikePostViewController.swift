f0b52ac0193dc31381918c653b4a734d1066fffa
LEESANGNAM <fgh0116@naver.com>
2024-02-15 22:10:36 +0900

    [Fix] myPage Cell 변경

--- SCloset/Scene/MyPage/Main/View/TabManView/MyLikePostViewController.swift
+++ SCloset/Scene/MyPage/Main/View/TabManView/MyLikePostViewController.swift
@@ -14,7 +14,7 @@ class MyLikePostViewController: BaseViewController {
     let viewModel = MyLikePostViewModel()
     lazy var collectionView:  UICollectionView = {
         let cv = UICollectionView(frame: .zero, collectionViewLayout: setCollectionViewLayout())
-        cv.register(PostImageCollectionViewCell.self, forCellWithReuseIdentifier: PostImageCollectionViewCell.identifier)
+        cv.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
         cv.delegate = self
         cv.dataSource = self
         cv.isScrollEnabled = false
@@ -63,9 +63,9 @@ class MyLikePostViewController: BaseViewController {
         let layout = UICollectionViewFlowLayout()
         let spacing: CGFloat = 10
         // 전체 너비 가져와서 빼기
-        let width = UIScreen.main.bounds.width - (spacing * 4)
-        let itemWidth = width / 3
-        layout.itemSize = CGSize(width: itemWidth, height: itemWidth * 1.3)
+        let width = UIScreen.main.bounds.width - (spacing * 3)
+        let itemWidth = width / 2
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth * 1.5)
         //컬렉션뷰 inset
         layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: 0, right: spacing)
         // 최소 간격
@@ -81,9 +81,9 @@ extension MyLikePostViewController: UICollectionViewDelegate, UICollectionViewDa
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostImageCollectionViewCell.identifier, for: indexPath) as? PostImageCollectionViewCell else { return UICollectionViewCell() }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as? HomeCollectionViewCell else { return UICollectionViewCell() }
         let data = viewModel.getPostData(index: indexPath.row)
-        cell.setData(data.toPostInfo())
+        cell.setData(data: data.toPostInfo())
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
